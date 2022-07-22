6a9d1a627bdc9e6b99979efd6cb893cdcd29c60b
Ryagard <ryn.goward@gmail.com>
2022-07-22 17:36:08 +0900

    Implemented Drawing
    Grid now displays in square. User can toggle grid size with slider.
    Hovering over cell will paint it black.

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-for (let i = 0; i < 256; i++) {
-    const grid = document.querySelector('.grid');
-    const cell = document.createElement('div');
-    cell.className = 'cell';
-    grid.appendChild(cell);
+let resolution = 16;
+const grid = document.querySelector('.grid-container');
+const options = document.querySelector('.options');
+const resValue = document.querySelector('.resValue');
+
+function drawGrid(res){
+    grid.style.gridTemplateColumns = `repeat(${res}, 1fr)`;
+    grid.style.gridTemplateRow = `repeat(${res}, 1fr)`;
+
+    for (let i = 0; i < (res * res); i++) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.addEventListener("mouseover", drawCell);
+        grid.appendChild(cell);
+    }
+    resValue.innerText = `${res} x ${res}`;
+}
+drawGrid(resolution);
+
+
+function changeRes(res){
+
+    while(grid.lastElementChild)
+    {
+        grid.removeChild(grid.lastElementChild);
+    }
+    
+    let resolution = res;
+
+    drawGrid(res);
+}
+
+function drawCell(e){
+    e.target.style.backgroundColor = "black";
 }
